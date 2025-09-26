4d991228f524b78c9552da5f88c4dfbd2c0bcae1
zerokqx <swiftkey382@gmail.com>
2025-09-27 00:33:04 +0300

    new plug and hot keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
+local mF = require("functions.miniFiles")
 vim.keymap.set("i", "<CL>", "<Esc>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<C-S-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
@@ -10,3 +10,5 @@ vim.keymap.set("n", "<C-S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decre
 vim.keymap.set("n", "<C-S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 -- vim.keymap.set("n", "<Leader>cn", ":lua require('neogen').generate()<CR>", { desc = "Neogen" })
 vim.keymap.set("n", "<Leader>rt", ":terminal bun run %<CR>", { desc = "Launch Typescript" })
+vim.keymap.set("n", "<Leader>e", mF.miniFilesRelative, { desc = "Launch Typescript" })
+vim.keymap.set("n", "<Leader>E", mF.miniFilesRoot, { desc = "Launch Typescript" })
